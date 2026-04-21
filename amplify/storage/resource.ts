4d162a2daf2ebbcade77f84c378245cf5e2f4ac9
bams634472 <sambamidele371@gmail.com>
2026-04-21 15:13:19 +0100

    updating storage folder

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,10 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'dropboxDrive',
+  name: 'dropcloudFiles',
   access: (allow) => ({
-    'media/{entity_id}/*': [
-      allow.authenticated.to(['read', 'write', 'delete'])
-    ]
-  })
+    'files/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write', 'delete']),
+    ],
+  }),
 });
